8cc9de9818821d01526be42df53a11a38a7c2846
m_owaki <m_owaki@nifty.com>
2026-03-08 12:32:52 +0900

    ログイン画面を修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,23 @@ import Login from './Login';
 import Home from './Home';
 
 function App() {
-  // ログイン状態のチェック（トークンの有無）
+  // ログイン状態のチェック
   const isAuthenticated = !!sessionStorage.getItem('kuroco_token');
 
   return (
     <Router>
       <Routes>
-        {/* ドメイン直下をHomeに設定 */}
+        {/* ドメイン直下を唯一のHOMEとする */}
         <Route 
           path="/" 
-          element={isAuthenticated ? <Home /> : <Navigate to="/Login" />} 
+          element={isAuthenticated ? <Home /> : <Navigate to="/Login" replace />} 
         />
         
         {/* ログイン画面 */}
         <Route path="/Login" element={<Login />} />
 
-        {/* 以前の /Home も残しておくか、不要なら削除してOKです */}
-        <Route 
-          path="/Home" 
-          element={isAuthenticated ? <Home /> : <Navigate to="/Login" />} 
-        />
-
-        {/* 定義されていないURLに来た場合はルートへ飛ばす */}
-        <Route path="*" element={<Navigate to="/" />} />
+        {/* それ以外のURL（/Homeなど）に直接来ても、すべてルート（/）へ飛ばす */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
