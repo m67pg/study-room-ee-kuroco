ec4abcf0142baa8b670e6f1cd3bce09d0ec19672
m_owaki <m_owaki@nifty.com>
2026-03-08 14:11:32 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,15 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './Login';
 import Home from './Home';
 
+// デバッグ用：認証を無視してHomeを出す
 function App() {
-  // セッションストレージからトークンを取得
-  // ブラウザをリロードしてもここが再評価されます
-  const token = sessionStorage.getItem('kuroco_token');
-  const isAuthenticated = !!token;
-console.log('Test');
-console.log(isAuthenticated);
-
   return (
     <Router>
       <Routes>
-        {/* 1. ルート (/) の挙動 */}
-        <Route 
-          path="/" 
-          element={
-            isAuthenticated ? <Home /> : <Navigate to="/Login" replace />
-          } 
-        />
-        
-        {/* 2. ログイン画面 (/Login) */}
-        {/* すでにログイン済みでここに来た場合はルート(/)へ戻す */}
-        <Route 
-          path="/Login" 
-          element={
-            !isAuthenticated ? <Login /> : <Navigate to="/" replace />
-          } 
-        />
-
-        {/* 3. 以前の残骸や打ち間違いなど、あらゆるURLをルートに集約 */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
 }
-
 export default App;
